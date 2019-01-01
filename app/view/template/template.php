d5ab34621330a209e7f998b973b111f8c1c5acae
MariuszWM <mariusz.marciniak@tuxedomoney.com>
2019-01-01 14:11:53 +0000

    template has been added

---
+++ app/view/template/template.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Pineapple Pizza Club</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <link rel="stylesheet" href="https://bootswatch.com/4/yeti/bootstrap.css" media="screen">
+    <link rel="stylesheet" href="https://bootswatch.com/yeti/_assets/css/custom.min.css">
+  </head>
+  <body class="container">
+
+
+    <h1>Hallo world!!</h1>
+
+    
+
+
+
+    <script src="https://bootswatch.com/yeti/_vendor/jquery/dist/jquery.min.js"></script>
+    <script src="https://bootswatch.com/yeti/_vendor/popper.js/dist/umd/popper.min.js"></script>
+    <script src="https://bootswatch.com/yeti/_vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="https://bootswatch.com/yeti/_assets/js/custom.js"></script>
+  </body>
+</html>
